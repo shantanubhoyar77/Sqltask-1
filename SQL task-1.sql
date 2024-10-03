461f1ed568db3841665d035356608fd2bab5872a
shantanubhoyar77 <pskshantanumernstack07@gmail.com>
2024-10-03 13:16:52 +0530

    Create SQL task-1.sql

---
+++ SQL task-1.sql
@@ -0,0 +1,88 @@
+Create table Managing_data (
+	id serial primary key,
+	name varchar(150),
+	age int,
+	location varchar,
+	Qualified varchar,
+    passout_year int,
+	status varchar,
+	result decimal
+
+)
+
+	select * from Managing_data
+
+INSERT INTO Managing_data (id, name, age, location, Qualified, passout_year, status, result)
+VALUES (1, 'Shantanu', 27, 'Nagpur','BE', 2019, 'married', 60);
+
+Insert INTO Managing_data (id, name, age, location, Qualified, passout_year, status, result)
+VALUES (2, 'Pranit',23,'sihore','MBA',2022,'unmarried',60);
+
+INSERT INTO Managing_data (id, name, age, location, Qualified, passout_year, status, result)
+VALUES (3,'Rahul', 34,  'nagpur', 'MBBS', 2010, 'married', 60);
+
+INSERT INTO Managing_data (id, name, age, location, Qualified, passout_year, status, result)
+VALUES (4,'pankaj', 36,  'nagpur', 'BE', 2018, 'married', 50);
+
+INSERT INTO Managing_data (id, name, age, location, Qualified, passout_year, status, result)
+VALUES (5,'priyanka', 32,  'nagpur', 'BSE', 2012, 'married', 40);
+
+
+INSERT INTO Managing_data values (6,'payal', 30,  'nagpur', 'BE', 2015, 'married', 65);
+
+INSERT INTO Managing_data values (7,'bhagyrashree', 22,  'nagpur', 'BE', 2022, 'unmarried', 75);
+
+INSERT INTO Managing_data values (8,'harsh', 21,  'nagpur', 'BCOM', 2023, 'unmarried', 45);
+
+INSERT INTO Managing_data values (9,'Tushar', 22,  'Amravati', 'BCOM', 2012, 'married', 90);
+
+INSERT INTO Managing_data values (10,'Akshay', 31,  'Amravati', 'BCOM', 2014, 'married', 80);
+
+INSERT INTO Managing_data values (11,'Shailash', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+
+Select * from Managing_data
+
+
+INSERT INTO Managing_data values (12,'S1', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (13,'S2', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (14,'S3', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Managing_data values (15,'S4', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (16,'S5', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (17,'S6', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (18,'S7', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (19,'S8', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Managing_data values (20,'S9', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+
+INSERT INTO Managing_data values (21,'S20', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (22,'S21', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (23,'S23', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Managing_data values (24,'S24', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (25,'S25', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (26,'S26', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (27,'S27', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (28,'S28', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Managing_data values (29,'S29', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (30,'S31', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (31,'S32', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (32,'S33', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Managing_data values (33,'S34', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (34,'S35', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (35,'S36', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (36,'S37', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (37,'S38', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Managing_data values (38,'S39', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (39,'S31', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (40,'S42', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (41,'S43', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Managing_data values (42,'S44', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (43,'S45', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (44,'S46', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (45,'S47', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (46,'S48', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Managing_data values (47,'S49', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (48,'S46', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Managing_data values (49,'S47', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Managing_data values (50,'S48', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Managing_data values (51,'S49', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+
+select * from Managing_data
